88b1ffa36ae45726919c92acd3997e10ce49e416
Jonah Thompson <jonah.thompson@mymail.champlain.edu>
2023-12-14 22:49:48 -0500

    Touch ups, resize buttons, save and exit buttons

--- main.cpp
+++ main.cpp
@@ -37,6 +37,8 @@ int main(int argc, char* argv[]) {
     playerController.inventory.resize(40);
     dialogController.inventoryPtr = &(playerController.inventory);
 
+    ImVec2 buttSize =  ImVec2(300,75);
+    ImVec2 winSize = ImVec2(400,300);
 
     // Unused argc, argv
     (void) argc;
@@ -71,39 +73,18 @@ int main(int argc, char* argv[]) {
         SDL_Log("Error creating SDL_Renderer!");
         abort();
     }
-    //SDL_RendererInfo info;
-    //SDL_GetRendererInfo(renderer, &info);
-    //SDL_Log("Current SDL_Renderer: %s", info.name);
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer_Init(renderer);
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
 
     // Our state
     bool show_demo_window = false;
@@ -128,16 +109,6 @@ int main(int argc, char* argv[]) {
     squareRect.x = width / 2 - squareRect.w / 2;
     squareRect.y = height / 2 - squareRect.h / 2;
 
-    //
-    //
-    //
-    //////
-    //
-    //
-    //
-
-
-
     // Event loop
     while (!done) {
         // Poll and handle events (inputs, window resize, etc.)
@@ -161,59 +132,26 @@ int main(int argc, char* argv[]) {
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        //showing the menu
+        //showing the main menu
         if(show_menu_window){
-            ImGui::Begin("Main Menu");
-            if(ImGui::Button("Load Game")){
+            ImGui::SetNextWindowSize(winSize);
+            ImGui::Begin("Main Menu",NULL,ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoCollapse);
+            if(ImGui::Button("Load Game",buttSize)){
                 show_save_window = true;
                 show_menu_window = false;
+            }
+            if(ImGui::Button("Close Game",ImVec2(300,25))){
+                exit(0);
             }
             ImGui::End();
         }
 
-        //showing the menu
+        //showing the save menu
         if(show_save_window){
-            bool wow = true;
             int saveSelection = 0;
-            ImGui::Begin("Saves");
-            if(ImGui::Button("Load Save One")){
+            ImGui::SetNextWindowSize(winSize);
+            ImGui::Begin("Saves",NULL,ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoCollapse);
+            if(ImGui::Button("Load Save One", buttSize)){
 
                 saveSelection = 1;
                 ifstream fin("savefiles/saveFile1.txt");
@@ -243,16 +181,19 @@ int main(int argc, char* argv[]) {
                 fin.close();
                 show_game_window = true;
                 show_save_window = false;
-            } else if(ImGui::Button("Load Save Two")) {
+            } else if(ImGui::Button("Load Save Two",buttSize)) {
                 show_game_window = true;
                 show_save_window = false;
                 saveSelection = 2;
-            } else if(ImGui::Button("Load Save Three")) {
+            } else if(ImGui::Button("Load Save Three",buttSize)) {
                 show_game_window = true;
                 show_save_window = false;
                 saveSelection = 3;
             }
-            //cout << "this is good\n";
+            if(ImGui::Button("Close Game",ImVec2(300,25))){
+                exit(0);
+            }
+
             //send save selection to story.cpp
             if (saveSelection == 1) {
                 saveFileName = "saveFile1.txt";
@@ -261,15 +202,13 @@ int main(int argc, char* argv[]) {
             } else if (saveSelection == 3) {
                 saveFileName = "saveFile3.txt";
             }
-            if(show_save_window == false) {
-
-            }
 
             ImGui::End();
         }
 
         //game window maybeeee :3
         if (show_game_window){
+            ImGui::SetNextWindowSize(winSize);
             //call story.cpp to get data of what to show on screen
             string allItems;
             dialogController.displayInfo();
@@ -278,39 +217,37 @@ int main(int argc, char* argv[]) {
             std::string buttonTwo = dialogController.dialog[2];
             std::string buttonThree = dialogController.dialog[3];
 
-
-            ImGui::Begin("Game Window");
+            ImGui::Begin("Game Window",NULL,ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoCollapse);
 
             //display description
 
             //send back player input when u get it
-
-            ImGui::Text("This is where the game is lol");
             ImGui::Text(roomDescription.c_str());// Display some text (you can use a format strings too)
 
             //get inventory items
-            //stops at 39 for some reason before going again
-           // for(int i = 0; i < playerController.inventory.size(); i++){
-                //cout << "iteration " + to_string(i) << endl;
-                //allItems += playerController.inventory.at(0) + "\n";
-           // }
-            //ImGui::Text(playerController.inventory.at(0).c_str());
+
             //cout << "this is item number 1: <" << playerController.inventory.at(0) << ">\n" ;
-            if (buttonOne != "NA\n" && ImGui::Button(buttonOne.c_str())) {
+            if (buttonOne != "NA\n" && ImGui::Button(buttonOne.c_str(),buttSize)) {
                 dialogController.changePage(1); //selection = 1;
             }
 
-            if (buttonTwo != "NA\n" && ImGui::Button(buttonTwo.c_str())) {
+            if (buttonTwo != "NA\n" && ImGui::Button(buttonTwo.c_str(),buttSize)) {
                 dialogController.changePage(2); //selection = 2;
             }
 
-            if ( buttonThree != "NA\n" && ImGui::Button(buttonThree.c_str())) {
+            if ( buttonThree != "NA\n" && ImGui::Button(buttonThree.c_str(),buttSize)) {
                 dialogController.changePage(3); //selection = 3;
+            }
+            if (ImGui::Button("Save",ImVec2(300,25))) {
+                dialogController.changePage(3); //selection = 3;
+            }
+
+            if(ImGui::Button("Close Game",ImVec2(300,25))){
+                exit(0);
             }
 
             ImGui::End();
         }
-
 
         // Rendering
         ImGui::Render();
@@ -318,24 +255,11 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
         SDL_RenderClear(renderer);
 
-        // todo: add your game logic here to be drawn before the ui rendering
-
         //call story.cpp to get data of what to show on screen
         std::string roomDescription = "ROOM DESCRIPTION HERE!!!!";
         std::string buttonOne = "BUTTON DESCRIPTION HERE";
         std::string buttonTwo = "BUTTON DESCRIPTION HERE";
         std::string buttonThree = "NA";
-        //display buttons
-
-        //display description
-
-        //send back player input when u get it
-
-
-        // Set renderer color red to draw the square
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-        // Draw filled square
-        SDL_RenderFillRect(renderer, &squareRect);
 
         // present ui on top of your drawings
         ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
